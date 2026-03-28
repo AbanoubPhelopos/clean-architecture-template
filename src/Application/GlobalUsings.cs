ba4f8b94d07ec17c3ee8b7b78f3e219a5e536661
abanoub saweris <abanoubphelopos12@gmail.com>
2026-03-28 22:53:11 +0200

    feat: add global using directives for cleaner code

---
+++ src/Application/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Application.Abstractions.Authentication;
+global using Application.Abstractions.Authorization;
+global using Application.Abstractions.Data;
+global using Application.Abstractions.Messaging;
+global using Application.Abstractions.Repositories;
+global using Application.Configuration;
+global using SharedKernel;
